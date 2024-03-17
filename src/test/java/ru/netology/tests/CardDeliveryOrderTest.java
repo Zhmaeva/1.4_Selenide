47b5f07bca0189ec7eb9503f148aac6e562ecaaa
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-03-17 12:43:07 +0300

    Add empty form test

--- src/test/java/ru/netology/tests/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/tests/CardDeliveryOrderTest.java
@@ -2,10 +2,12 @@ package ru.netology.tests;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -14,25 +16,60 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
 
-    LocalDate date = LocalDate.now();
+    //selectors
+    String validDateSelector = "[data-test-id='date'] input";
+    String validCitySelector = "[data-test-id='city'] input";
+    String validNameSelector = "[data-test-id='name'] input";
+    String validPhoneSelector = "[data-test-id='phone'] input";
+    String validAgreementSelector = "[data-test-id='agreement'] span.checkbox__box";
+    String notificationTitleSelector = "[data-test-id=notification] .notification__title";
+    String notificationContentSelector = "[data-test-id=notification] .notification__content";
+    String invalidCitySelector = "[data-test-id=city].input_invalid .input__sub";
+    String invalidDateSelector = "[data-test-id=date] .input_invalid .input__sub";
+    String invalidNameSelector = "[data-test-id=name].input_invalid .input__sub";
+    String invalidPhoneSelector = "[data-test-id=phone].input_invalid .input__sub";
+    String invalidAgreementSelector = "[data-test-id=agreement].input_invalid";
+
+    //messages
+    String notificationTitleText = "Успешно!";
+    String notificationContentText = "Встреча успешно забронирована на ";
+    String emptyFieldText = "Поле обязательно для заполнения";
+
+    String calculateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    @BeforeEach
+    void setUpAll() {
+        open("http://localhost:9999/");
+    }
 
     //end to end (состояние загрузки не более 15сек)
     @Test
-    void shouldSendCardOrderForm() throws InterruptedException {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date'] input").setValue(String.valueOf(date.plusDays(3)));
-        $("[data-test-id='name'] input").setValue("Иван Иванов-Петров");
-        $("[data-test-id='phone'] input").setValue("+70000000000");
-        $("[data-test-id='agreement']").click();
+    void shouldSendCardOrderForm() {
+        String date = calculateDate(3);
+        $(validCitySelector).setValue("Москва");
+        $(validDateSelector).setValue(date);
+        $(validNameSelector).setValue("Иван Иванов-Петров");
+        $(validPhoneSelector).setValue("+70000000000");
+        $(validAgreementSelector).click();
         $$("button").find(exactText("Забронировать")).click();
-        // $("fieldset").shouldBe(disabled);
-        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-
-
-
+        // $("fieldset").shouldBe(disabled); ?
+        $(notificationTitleSelector).shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text(notificationTitleText));
+        $(notificationContentSelector).shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text(notificationContentText + date));
     }
+
     //отправка пустой формы
+    @Test
+    void shouldNotSendEmptyForm() {
+        $(validCitySelector).clear();
+        $(validDateSelector).clear();
+        $(validNameSelector).clear();
+        $(validPhoneSelector).clear();
+        $(validAgreementSelector).isEnabled();
+        $$("button").find(exactText("Забронировать")).click();
+        $(invalidCitySelector).shouldBe(visible).shouldHave(text(emptyFieldText));
+    }
+
     //поле ГОРОД не заполнено
     //поле ДАТА ВСТРЕЧИ не заполнено
     //поле ФИО не заполнено
